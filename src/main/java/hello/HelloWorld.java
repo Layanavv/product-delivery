e9f32c60c7211f7bf24fe7eeed440b96f85cad3c
Layanavv <2021wb86018@wilp.bits-pilani.ac.in>
2025-07-13 00:10:09 +0530

    Changes made in HelloWorld.java file

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("I am making Chnages here: " + currentTime);
+		  System.out.println("I made Chnages here: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
